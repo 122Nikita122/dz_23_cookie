7087e3a4bd7ffc0cc71f99d916d1ad976237e982
Nikita <sidorow.nikta8144943@yandex.ru>
2020-11-25 16:45:01 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Cookies/>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Cookies from './components/Cookies.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Cookies
   }
 }
 </script>
---
+++ src/components/Cookies.vue
@@ -0,0 +1,31 @@
+<template>
+  <div >
+    <h1>Столько то раз нажал на печеньку {{count}}</h1>
+    <img class="cookie"  v-bind:src="src"  v-on:click="counter">    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Cookies',
+    data(){
+      return {
+        count: 0, 
+        src: '../assets/cookie.jpg',
+      };
+    },  
+  methods:{
+    counter(){
+      return this.count++
+    }
+  }
+
+}
+</script>
+
+<style>
+.cookie {
+  height: 200px;  
+}
+</style>
